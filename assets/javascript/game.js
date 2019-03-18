17ed3e9fb63c507f92e9b45f55e3ac3077a90ea3
camiwins <camiwins@gmail.com>
2019-03-17 22:59:03 -0700

    filled in more pseudocode and tried adding some lines in. not happy with it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,62 @@
 
 // Variable objects of all the words the computer can select
 
+var wordOptions = [
+    "sushi",
+    "sashimi",
+    "ramen",
+    "tempura",
+    "miso soup",
+    "shabu shabu",
+    "yakitori",
+    "tsukemen",
+    "onigiri",
+    "udon",
+    "soba",
+    "gyoza",
+    "tonkatsu",
+    "wagyu",
+    "takoyaki",
+    "natto",
+    "kabayaki",
+]
+
 // Variable for the current word the computer has selected
 
-// Variable for the word that's being guessed by the user
+// Variable for the blank word that's being guessed by the user
+    var blankWord = [];
 
-// Variables that display the wins, losses, guesses and letters guessed
+// Variables that display the wins, losses, guesses and letters that the user has already guessed
     // Wins start at 0 - Counter for each time user guesses the word
-    // Losses start at 0 - Counter for losses once user makes max number of guessing attempts, aka errors
-    // Errors start at 15 - Each time a user can guess wrong
-    // Guessed is blank - For all the letters the cuser guessed
+    // Losses start at 0 - Counter for losses once user hits max number of guesses
+    // Guesses start at 15 - Each time a user can guess wrong
+    const guessesMax = 15;
+    // Letters is blank - For all the letters the cuser guessed
 
-// Variables for game states
-    // Game start
-    // Game over
+// Flags for beginning and end game states
+    // Game start - run the function that starts the game (might not need this, but will remove after testing)
+    var gameStart = false;
+    // Game over - prompts the user to try guessing the new word
+    var gameOver = false;
 
 // *** GAME FUNCTIONS AND KEY EVENTS
 
-// Function that starts the game with default counters
+// Function that resets the game with default score counters
+function resetScore() {
+    // Guesses 15 (max)
+    guessesLeft = guessMax;
+    // Game start
+    gameStart
 
-// Computer randomly chooses a word from the list mentioned above
-    // Display a series of blanks that match the length of the word computer chose
+    // Computer randomly chooses a word from the list mentioned above
+        // Display a series of blanks that match the length of the word computer chose
+        var blankWord = Math.floor(Math.random() * (wordOptions.length));
+
+    // Create the blanks for the word the user has to guess
+
+    // Hide image win word and game over text
+
+    // Show everything
 
 // User attempts to guess what the word is by pressing letters on the keyboard
 
